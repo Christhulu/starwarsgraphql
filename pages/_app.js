894a892d12f908c6e81e3f642302ef68e8d271fe
Chris Alexander <Christopher.J.Alexander@Vanderbilt.edu>
2022-04-27 10:10:47 -0400

    Added ships page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
-import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/themes/lara-dark-teal/theme.css'
+import "primereact/resources/primereact.min.css";     
+import '/node_modules/primeflex/primeflex.css'
+import "primeicons/primeicons.css";     
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
